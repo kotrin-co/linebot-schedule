4bb699c8b9c8db9c003edb26805e2a55e060d0dd
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-18 22:49:21 +0900

    2nd confirm template

--- index.js
+++ index.js
@@ -155,47 +155,22 @@ const handleMessageEvent = async (ev) => {
                 });
                 setTimeout(()=>{
                   client.pushMessage(id,{
-                    "type":"flex",
-                    "altText":"date_selector",
-                    "contents":
-                    {
-                      "type": "bubble",
-                      "header": {
-                        "type": "box",
-                        "layout": "vertical",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "この予約を削除しますか？"
-                          }
-                        ]
-                      },
-                      "body": {
-                        "type": "box",
-                        "layout": "horizontal",
-                        "contents": [
-                          {
-                            "type": "button",
-                            "action": {
-                              "type": "postback",
-                              "label": "はい",
-                              "data": `delete-yes`
+                    "type": "template",
+                    "template": {
+                        "type": "confirm",
+                        "actions": [
+                            {
+                                "type": "postback",
+                                "label": "はい",
+                                "data": "delete-yes"
                             },
-                            "style": "primary",
-                            "margin": "lg"
-                          },
-                          {
-                            "type": "button",
-                            "action": {
-                              "type": "postback",
-                              "label": "いいえ",
-                              "data": `delete-no`
-                            },
-                            "style": "secondary",
-                            "margin": "lg"
-                          }
-                        ]
-                      }
+                            {
+                                "type": "postback",
+                                "label": "いいえ",
+                                "text": "delete-no"
+                            }
+                        ],
+                        "text": "この予約をキャンセルしますか？"
                     }
                   });
                 },1500);

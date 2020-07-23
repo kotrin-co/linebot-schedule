cb6cd9dc9063b45874e2aead0621d805f6036ee0
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-23 17:30:05 +0900

    13th button operation

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,7 @@ const today = new Date().getDate();
 const dayOfTheWeek = new Date().getDay();
 const nowTime = new Date(`${thisYear}-${thisMonth}-${today} 00:00`).getTime();
 const oneWeek = 604800000;
+const oneDay = 86400000;
 let index = 0;
 
 const getDateElements = (timestamp) => {
@@ -21,12 +22,13 @@ const getDateElements = (timestamp) => {
 const weeks = ['日','月','火','水','木','金','土'];
 
 const displayCalendar = () =>{
-    const ts = nowTime + oneWeek*index;
-    const dateArray = getDateElements(ts);
+    const base_ts = nowTime + oneWeek*index;
+    const dateArray = getDateElements(base_ts);
     h2Element.textContent = `${dateArray[0]}年${dateArray[1]}月`;
     for(let i=0;i<7;i++){
         const dayElement = document.getElementById(`d${i}`);
-        dayElement.textContent = `${dateArray[2]+i}`;
+        const date = new Date(base_ts+oneDay*i).getDate();
+        dayElement.textContent = `${date}`;
         const weekElement = document.getElementById(`w${i}`);
         if(dateArray[3]+i<7){
             weekElement.textContent = weeks[dateArray[3]+i];

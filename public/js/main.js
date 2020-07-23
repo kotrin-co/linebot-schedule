58311317c96adeee7bd141ece6626307d8a0dccc
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-23 09:50:32 +0900

    12th table element

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,17 @@
 const rButton = document.getElementById('rButton');
 const pButton = document.getElementById('pButton');
+const h2Element = document.getElementById('month');
+
+const thisMonth = new Date().getMonth+1;
+const today = new Date().getDate();
+
+const displayCalendar = () =>{
+    h2Element.textContent = `${thisMonth}`;
+    for(let i=0;i<7;i++){
+        const tdElement = document.getElementById(`d${i}`);
+        tdElement.textContent = `${today+i}`;
+    }
+}
 
 rButton.addEventListener('click',(event)=>{
     alert('Rボタンが押された！');
@@ -9,3 +21,4 @@ pButton.addEventListener('click',(event)=>{
     alert('Pボタンが押された！');
 });
 
+displayCalendar();

84fc3fc7de557ace5ba328b6490c4c2f096eacdf
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-23 12:09:03 +0900

    13th button operation

--- public/js/main.js
+++ public/js/main.js
@@ -2,33 +2,47 @@ const rButton = document.getElementById('rButton');
 const pButton = document.getElementById('pButton');
 const h2Element = document.getElementById('monthArea');
 
+const thisYear = new Date().getFullYear();
 const thisMonth = new Date().getMonth()+1;
 const today = new Date().getDate();
 const dayOfTheWeek = new Date().getDay();
+const nowTime = new Date(`${thisYear}-${thisMonth}-${today} 00:00`).getTime();
+const oneWeek = 604800000;
+let index = 0;
+
+const getDateElements = (timestamp) => {
+    const y = new Date(timestamp).getFullYear();
+    const m = new Date(timestamp).getMonth()+1;
+    const d = new Date(timestamp).getDate();
+    const w = new Date(timestamp).getDay();
+    return [y,m,d,w];
+}
 
 const weeks = ['日','月','火','水','木','金','土'];
 
 const displayCalendar = () =>{
-    h2Element.textContent = `${thisMonth}月`;
+    const ts = nowTime + oneWeek*index;
+    const dateArray = getDateElements(ts);
+    h2Element.textContent = `${dateArray[0]}年${dateArray[1]}月`;
     for(let i=0;i<7;i++){
         const dayElement = document.getElementById(`d${i}`);
-        dayElement.textContent = `${today+i}`;
+        dayElement.textContent = `${dateArray[2]+i}`;
         const weekElement = document.getElementById(`w${i}`);
-        if(dayOfTheWeek+i<7){
-            weekElement.textContent = weeks[dayOfTheWeek+i];
-            if(dayOfTheWeek+i === 0){
+        if(dateArray[3]+i<7){
+            weekElement.textContent = weeks[dateArray[3]+i];
+            if(dateArray[3]+i === 0){
                 dayElement.setAttribute('title','red');
                 weekElement.setAttribute('title','red');
-            }else if(dayOfTheWeek+i === 6){
+            }else if(dateArray[3]+i === 6){
                 dayElement.setAttribute('title','blue');
                 weekElement.setAttribute('title','blue');
             }
         }else{
-            weekElement.textContent = weeks[dayOfTheWeek+i-7];
-            if(dayOfTheWeek+i-7 === 0){
+            weekElement.textContent = weeks[dateArray[3]+i-7];
+            if(dateArray[3]+i-7 === 0){
                 dayElement.setAttribute('title','red');
                 weekElement.setAttribute('title','red');
-            }else if(dayOfTheWeek+i-7 === 6){
+            }else if(dateArray[3]+i-7 === 6){
                 dayElement.setAttribute('title','blue');
                 weekElement.setAttribute('title','blue');
             }
@@ -37,11 +51,13 @@ const displayCalendar = () =>{
 }
 
 rButton.addEventListener('click',(event)=>{
-    alert('Rボタンが押された！');
+    index--;
+    displayCalendar();
 });
 
 pButton.addEventListener('click',(event)=>{
-    alert('Pボタンが押された！');
+    index++;
+    displayCalendar();
 });
 
 displayCalendar();

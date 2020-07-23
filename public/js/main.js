6c1018a673566ee96bf01c7a384670210ea17503
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-23 10:22:02 +0900

    12th table element

--- public/js/main.js
+++ public/js/main.js
@@ -9,12 +9,12 @@ const dayOfTheWeek = new Date().getDay();
 const weeks = ['日','月','火','水','木','金','土'];
 
 const displayCalendar = () =>{
-    h2Element.textContent = `${thisMonth}`;
+    h2Element.textContent = `${thisMonth}月`;
     for(let i=0;i<7;i++){
         const dayElement = document.getElementById(`d${i}`);
         dayElement.textContent = `${today+i}`;
         const weekElement = document.getElementById(`w${i}`);
-        if(w+i<7){
+        if(dayOfTheWeek+i<7){
             weekElement.textContent = weeks[dayOfTheWeek+i];
         }else{
             weekElement.textContent = weeks[dayOfTheWeek+i-7];

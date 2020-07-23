8ed3168b30b1992c2ce7402e6175aee7f14907d7
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-23 10:10:25 +0900

    12th table element

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,25 @@
 const rButton = document.getElementById('rButton');
 const pButton = document.getElementById('pButton');
-const h2Element = document.getElementById('month');
+const h2Element = document.getElementById('monthArea');
 
-const thisMonth = new Date().getMonth+1;
+const thisMonth = new Date().getMonth()+1;
 const today = new Date().getDate();
+const dayOfTheWeek = new Date().getDay();
+
+const weeks = ['日','月','火','水','木','金','土'];
 
 const displayCalendar = () =>{
     h2Element.textContent = `${thisMonth}`;
     for(let i=0;i<7;i++){
-        const tdElement = document.getElementById(`d${i}`);
-        tdElement.textContent = `${today+i}`;
+        const dayElement = document.getElementById(`d${i}`);
+        dayElement.textContent = `${today+i}`;
+        const weekElement = document.getElementById.length(`w${i}`);
+        let w = dayOfTheWeek;
+        if(w+i<7){
+            weekElement.textContent = weeks[w+i];
+        }else{
+            weekElement.textContent = weeks[w+i-7];
+        }
     }
 }
 

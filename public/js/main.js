a546b86f69dcc11a4058ac7d89a682088284eb08
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-23 09:11:05 +0900

    12th table element

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
-window.addEventListener('load',()=>{
-    alert('main.jsを読み込んだ！');
+const rButton = document.getElementById('rButton');
+const pButton = document.getElementById('pButton');
+
+rButton.addEventListener('click',(event)=>{
+    alert('Rボタンが押された！');
 });
+
+pButton.addEventListener('click',(event)=>{
+    alert('Pボタンが押された！');
+});
+

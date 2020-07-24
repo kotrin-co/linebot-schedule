b7c3f72bf74aff4464f3d70bd0beeb8ee4225df1
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-24 22:56:50 +0900

    3rd commit in develop

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ const rButton = document.getElementById('rButton');
 const pButton = document.getElementById('pButton');
 const h2Element = document.getElementById('monthArea');
 const wButton = document.getElementById('thisWeekButton');
-// const reservations = [];
+const reservations = null;
 
 const tdElement = document.getElementById('d0-10');
 
@@ -27,9 +27,9 @@ window.addEventListener('load',(event)=>{
 const fetchData = async () =>{
     try{
         const response = await fetch(API_URL);
-        const reservations = await response.json();
-        console.log('response.json:',reservations);
-        tdElement.textContent = getReservationDisplay(reservations[0].starttime,reservations[0].name,reservations[0].menu);
+        const data = await response.json();
+        reservations = data;
+        console.log('reservations:',reservations);
     }catch(error){
         alert(`読み込み失敗...${error.message}`);
     }

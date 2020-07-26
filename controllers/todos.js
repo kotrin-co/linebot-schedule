e93d98c1859cc0d54f5b432f0943022719935b27
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-26 11:00:39 +0900

    11th in develop

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,14 +2,10 @@ const Todo = require('../models/Todo');
 
 module.exports = {
     getTodos: (req,res) => {
-        console.log('getTodosの頭');
         Todo.findAll()
             .then(reservations=>{
-                console.log('getTodos実行。findAllからのプロミス返りthenの中');
                 res.status(200).json(reservations);
             })
             .catch(e=>console.log(e.stack));
-        // const storedTodos = Todo.findAll();
-        // res.status(200).json(storedTodos);
     }
 }
--- models/Todo.js
+++ models/Todo.js
@@ -8,88 +8,6 @@ const connection = new Client({
   });
 connection.connect();
 
-// const adminData = {
-//     users:null,
-//     reservations:null
-// };
-
-// const pickup_users = {
-//     text:'SELECT * FROM users ORDER BY id ASC;'
-// };
-// const pickup_reservations = {
-//     text:'SELECT * FROM schedules ORDER BY starttime ASC;'
-// };
-// connection.query(pickup_users)
-//     .then(res=>{
-//         // console.log('users:',res.rows);
-//         adminData.users = res.rows;
-//         connection.query(pickup_reservations)
-//             .then(res=>{
-//                 // console.log('reservations:',res.rows);
-//                 adminData.reservations = res.rows;
-//                 adminData.reservations.map(object=>{
-//                     object.starttime = parseInt(object.starttime);
-//                     object.endtime = parseInt(object.endtime);
-//                 });
-//                 console.log('Todo.jsの外側クエリ実行');
-//             })
-//             .catch(e=>console.log(e.stack));
-//     })
-//     .catch(e=>console.log(e.stack));
-
-// pickupAllReservations()
-//     .then(message=>{
-//         console.log('message:',message);
-//         console.log('adminData:',adminData.reservations;
-//     })
-//     .catch(e=>console.log(e.stack));
-
-// const pickupAllReservations = () => {
-//     return new Promise((resolve,reject)=>{
-//         const pickup_users = {
-//             text:'SELECT * FROM users ORDER BY id ASC;'
-//         };
-//         const pickup_reservations = {
-//             text:'SELECT * FROM schedules ORDER BY starttime ASC;'
-//         };
-//         connection.query(pickup_users)
-//             .then(res=>{
-//                 console.log('users:',res.rows);
-//                 adminData.users = res.rows;
-//                 connection.query(pickup_reservations)
-//                     .then(res=>{
-//                         console.log('reservations:',res.rows);
-//                         adminData.reservations = res.rows;
-//                         resolve('select_query 成功！！');
-//                     })
-//                     .catch(e=>console.log(e.stack));
-//             })
-//             .catch(e=>console.log(e.stack));
-//     });
-// }
-
-// const todos = [];
-
-// let nextId = 1;
-
-// class Todo {
-//     constructor({title,body}){
-//         this.id = nextId++;
-//         this.title = title;
-//         this.body = body;
-//         this.createdAt = new Date();
-//         this.updatedAt = new Date();
-//     }
-// }
-
-// for(let i=0;i<5;i++){
-//     const index = i+1;
-//     const todo = new Todo({
-//         title:'タイトル'+index,
-//         body:'ボディ'+index
-//     });
-//     todos.push(todo);
-// }
 
 module.exports = {
     findAll:()=>{
@@ -107,24 +25,19 @@ module.exports = {
             };
             connection.query(pickup_users)
                 .then(res=>{
-                    // console.log('users:',res.rows);
                     adminData.users = res.rows;
                     connection.query(pickup_reservations)
                         .then(res=>{
-                            // console.log('reservations:',res.rows);
                             adminData.reservations = res.rows;
                             adminData.reservations.map(object=>{
                                 object.starttime = parseInt(object.starttime);
                                 object.endtime = parseInt(object.endtime);
                             });
-                            console.log('findallの中、クエリ実行終了');
-                            resolve(adminData.reservations.slice());
-                            console.log('findallの中、resolve後');   
+                            resolve(adminData.reservations.slice()); 
                         })
                         .catch(e=>console.log(e.stack));
                 })
                 .catch(e=>console.log(e.stack));        
         });
-        // return adminData.reservations.slice();
     }
 };

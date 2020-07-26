59aaafeb733b82f2a3e7cd89fa83f063ac2cbffa
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-26 15:03:07 +0900

    12th in develop onclick

--- public/js/main.js
+++ public/js/main.js
@@ -76,7 +76,7 @@ const js_confirm = (num) => {
     const s_m = ('0'+new Date(target.starttime-oneHour*9).getMinutes()).slice(-2);
     const e_h = ('0'+new Date(target.endtime-oneHour*9).getHours()).slice(-2);
     const e_m = ('0'+new Date(target.endtime-oneHour*9).getMinutes()).slice(-2);
-    dialog_contents.innerHTML = `■予約id:${target.id}<br>■予約名:${target.name}<br>■予約日時:${month}月${date}日<br>　　　　${s_h}時${s_m}分〜${e_h}時${e_m}分<br>■メニュー:${target.menu}`
+    dialog_contents.innerHTML = `■予約id:${target.id}<br>■予約名:${target.name}<br>■予約日時:${month}月${date}日<br>　　　　 　${s_h}時${s_m}分〜${e_h}時${e_m}分<br>■メニュー:${target.menu}`
     dialog.style.display = 'block';
 }
 

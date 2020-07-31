3236772f84d7f4600ace802aefb0e584c636062f
user <user@rrcs-172-254-99-33.nyc.biz.rr.com>
2020-07-31 22:39:17 +0900

    14th create

--- controllers/todos.js
+++ controllers/todos.js
@@ -12,9 +12,14 @@ module.exports = {
     postTodo: (req,res) => {
         try{
             const {line_uid,name,year,date_m,date_d,starttime_h,starttime_m,menu} = req.body;
-            const createdReservation = Todo.create({line_uid,name,year,date_m,date_d,starttime_h,starttime_m,menu});
-            console.log('createdReservation:',createdReservation);
-            res.status(200).json(createdReservation);
+            Todo.create({line_uid,name,year,date_m,date_d,starttime_h,starttime_m,menu})
+                .then(message=>{
+                    res.status(200).json(message);
+                })
+                .catch(e=>console.log(e.stack));
+            // const createdReservation = Todo.create({line_uid,name,year,date_m,date_d,starttime_h,starttime_m,menu});
+            // console.log('createdReservation:',createdReservation);
+            // res.status(200).json(createdReservation);
          }catch(error){
              res.status(400).json({message:error.message});
          }
